a2cd3781605153d309c50583f6b125d0577c8aae
Wahaj <wahajdorift@yahoo.com>
2022-03-12 05:20:27 -0600

    integration of zoom

---
+++ laravel_video_app/app/Http/Controllers/ZoomController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Traits\ZoomMeetingTrait;
+use Illuminate\Http\Request;
+
+class ZoomController extends Controller
+{
+    use ZoomMeetingTrait;
+
+    const MEETING_TYPE_INSTANT = 1;
+    const MEETING_TYPE_SCHEDULE = 2;
+    const MEETING_TYPE_RECURRING = 3;
+    const MEETING_TYPE_FIXED_RECURRING_FIXED = 8;
+
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function show($id)
+    {
+        $meeting = $this->get($id);
+
+        return view('meetings.index', compact('meeting'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->create($request->all());
+
+        return redirect()->route('meetings.index');
+    }
+
+    public function update($meeting, Request $request)
+    {
+        $this->update($meeting->zoom_meeting_id, $request->all());
+
+        return redirect()->route('meetings.index');
+    }
+
+    public function destroy(ZoomMeetingTrait $meeting)
+    {
+        $this->delete($meeting->id);
+
+        return $this->sendSuccess('Meeting deleted successfully.');
+    }
+}
--- laravel_video_app/routes/web.php
+++ laravel_video_app/routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ZoomController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('', 'HomeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('meetings', 'ZoomController@index')->name('meetings.index');
+Route::get('start-meeting/{meeting}', 'ZoomController@start_meeting')->name('meeting.start');
+Route::get('join-meeting/{meeting}', 'ZoomController@join_meeting')->name('meeting.join');
+Route::get('leave-meeting', 'ZoomController@leave_meeting')->name('meeting.leave');
+Route::get('create-new-meeting', 'ZoomController@create')->name('meeting.create');
+Route::post('create-new-meeting', 'ZoomController@store')->name('meeting.store');
+Route::delete('delete-meeting/{meeting}', 'ZoomController@destroy')->name('meeting.destroy');
+Auth::routes();
+Route::get('/aa',function(){return 'wwww';});
+Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::resource('home', HomeController::class);
+// Route::resources('home',)
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
